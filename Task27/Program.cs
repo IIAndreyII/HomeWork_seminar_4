473068db4c4cdb5ceeba625285c312f21ea199d4
unknown <volk.3894@yandex.ru>
2022-09-05 16:09:07 +0300

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 27: Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+int temp = 0;
+
+while(num >0)
+{
+    temp = num % 10;
+    num /= 10;
+    count += temp;
+}
+Console.Write(count);
